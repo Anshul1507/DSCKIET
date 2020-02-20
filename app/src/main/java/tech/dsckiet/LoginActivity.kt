8c9840df6a018a1b24a5c578c92830a167d8a2a0
Anshul1507 <ag1507anshul@gmail.com>
2020-02-20 20:20:09 +0530

    update :: Register UI added
    added animations bw login and register screens

--- app/src/main/java/tech/dsckiet/LoginActivity.kt
+++ app/src/main/java/tech/dsckiet/LoginActivity.kt
@@ -1,9 +1,17 @@
 package tech.dsckiet
 
-import android.content.Intent
-import android.graphics.Color
+
 import android.os.Bundle
+import android.os.Handler
+import android.text.BoringLayout
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.MotionEvent
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.RotateAnimation
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.razir.progressbutton.attachTextChangeAnimator
 import com.github.razir.progressbutton.bindProgressButton
@@ -15,6 +23,7 @@ import kotlinx.android.synthetic.main.layout_registration.*
 
 class LoginActivity : AppCompatActivity() {
 
+    private var flag : Boolean= false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -34,6 +43,7 @@ class LoginActivity : AppCompatActivity() {
                 progressColors = arrayColors
             }
         }
+
         button_register.setOnClickListener {
             button_register.isClickable = false
             button_register.attachTextChangeAnimator()
@@ -43,16 +53,37 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
+        //Eye Icon Password Edit text
+        editText_login_password.setOnTouchListener { view, motionEvent -> passIcon(view,motionEvent) }
+        editText_register_password.setOnTouchListener { view, motionEvent -> passIcon(view,motionEvent) }
+
         text_login_to_register.setOnClickListener {
+            val rotate:Animation = AnimationUtils.loadAnimation(this, R.anim.slide_in_right)
+            register_image?.startAnimation(rotate)
+            section_register_form.startAnimation(rotate)
+            text_register_to_login.startAnimation(rotate)
             showRegistration()
         }
+
         text_register_to_login.setOnClickListener {
+            val rotate:Animation = AnimationUtils.loadAnimation(this, R.anim.slide_in_left)
+            login_image?.startAnimation(rotate)
+            section_login_form.startAnimation(rotate)
+            text_login_to_register.startAnimation(rotate)
             showLogin()
         }
-        
+
         fab_skip_login.setOnClickListener {
+            Handler().postDelayed({
                 finish()
                 overridePendingTransition(R.anim.no_animation,R.anim.slide_down)
+            }, 100)
+        }
+        fab_skip_register.setOnClickListener {
+            Handler().postDelayed({
+                finish()
+                overridePendingTransition(R.anim.no_animation,R.anim.slide_down)
+            }, 100)
         }
     }
 
@@ -64,5 +95,28 @@ class LoginActivity : AppCompatActivity() {
     private fun showLogin(){
         layout_register.visibility = View.INVISIBLE
         layout_login.visibility = View.VISIBLE
+    }
+    private fun passIcon(view: View,event: MotionEvent): Boolean{
+        var DRAWABLE_LEFT = 0
+        var DRAWABLE_TOP = 1
+        val DRAWABLE_RIGHT  = 2
+        var DRAWABLE_BOTTOM = 3
+        if(event.action == MotionEvent.ACTION_UP) {
+            if(event.rawX >= (editText_login_password.right - editText_login_password.compoundDrawables[DRAWABLE_RIGHT].bounds.width())) {
+                if (!flag){
+                    //close pass eye icon
+                    flag = true
+                    editText_login_password.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_pass_eye_open, 0);
+                    editText_login_password.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                }else{
+                    //open pass eye icon
+                    flag = false
+                    editText_login_password.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_pass_eye_close, 0);
+                    editText_login_password.transformationMethod = PasswordTransformationMethod.getInstance()
+                }
+                return true
+            }
+        }
+        return false
     }
 }
